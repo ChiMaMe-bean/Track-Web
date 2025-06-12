21a8923bac3de534acf1ebdc19d2ab8a8a275dd9
ChiMaMe-bean <dolphin202307@163.com>
2025-06-12 17:43:34 +0800

    Track-Web 1.0.0

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -7,6 +7,7 @@ import { defineClientConfig } from 'vuepress/client'
 // import CustomComponent from './theme/components/Custom.vue'
 
 import './theme/styles/custom.css'
+import $ from 'jquery';
 
 export default defineClientConfig({
   enhance({ app }) {
@@ -20,3 +21,15 @@ export default defineClientConfig({
     // app.component('CustomComponent', CustomComponent)
   },
 })
+
+$(function () {
+  $(window).on('scroll', function () {
+    const len: number = $(this).scrollTop() as number;
+    // 根据滚动位置决定显示或隐藏元素
+    if (len >= 400) {
+      $('.scroll-down-arrow').fadeOut(100); // 使用淡出效果
+    } else {
+      $('.scroll-down-arrow').fadeIn(100); // 使用淡入效果
+    }
+  });
+});
